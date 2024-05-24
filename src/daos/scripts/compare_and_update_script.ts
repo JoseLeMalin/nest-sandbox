55925e897a06a4fb20c3278babfcfe921502a737
JoseLeMalin <seb_488@hotmail.fr>
2024-05-24 12:00:28 +0200

    Working on Lua scripts

---
+++ src/daos/scripts/compare_and_update_script.ts
@@ -0,0 +1,100 @@
+import { redisClient } from "src/config/redis.config";
+
+let sha: string;
+
+/**
+ * Get the Lua source code for the script.
+ * @returns {string} - Lua source code for the script.
+ * @private
+ */
+const getSource = () => `
+  local key = KEYS[1]
+  local field = ARGV[1]
+  local value = ARGV[2] 
+  local op = ARGV[3]
+  local current = redis.call('hget', key, field)
+  if (current == false or current == nil) then
+    redis.call('hset', key, field, value)
+  elseif op == '>' then
+    if tonumber(value) > tonumber(current) then
+      redis.call('hset', key, field, value)
+    end
+  elseif op == '<' then
+    if tonumber(value) < tonumber(current) then
+      redis.call('hset', key, field, value)
+    end
+  end `;
+
+const load = async () => {
+  const client = (await redisClient()).redisClient;
+
+  // Load script on first use...
+  if (!sha) {
+    sha = await client.scriptLoad(getSource());
+  }
+
+  return sha;
+};
+
+/**
+ * Build up an array of parameters that will be passed to
+ * evalsha.
+ *
+ * @param {string} key - Redis key that the script will operate on.
+ * @param {string} field - Field name in the hash to use.
+ * @param {number} value - Value to set the field to if it passes the
+ *   comparison test.
+ * @param {string} comparator - '<' or '>' depending on whether the value
+ *   should be updated if less or greater than the existing value.
+ * @returns {Array} - array of parameters that evalsha can use to execute
+ *   the script.
+ * @private
+ */
+const buildEvalshaParams = (key: string, field: string, value: string, comparator: string) => [
+  sha, // Script SHA
+  1, // Number of Redis keys
+  key,
+  field,
+  value,
+  comparator,
+];
+
+const updateIfGreater = (key: string, field: string, value: string) =>
+  buildEvalshaParams(key, field, value, ">");
+
+const updateIfLess = (key: string, field: string, value: string) =>
+  buildEvalshaParams(key, field, value, "<");
+
+module.exports = {
+  /**
+   * Load the script into Redis and return its SHA.
+   * @returns {string} - The SHA for this script.
+   */
+  load,
+
+  /**
+   * Build up an array of parameters that evalsha will use to run
+   * a compare and set if greater operation.
+   *
+   * @param {string} key - Redis key that the script will operate on.
+   * @param {string} field - Field name in the hash to use.
+   * @param {number} value - Value to set the field to if it passes the
+   *   comparison test.
+   * @returns {Array} - array of parameters that evalsha can use to execute
+   *   the script.
+   */
+  updateIfGreater,
+
+  /**
+   * Build up an array of parameters that evalsha will use to run
+   * a compare and set if less operation.
+   *
+   * @param {string} key - Redis key that the script will operate on.
+   * @param {string} field - Field name in the hash to use.
+   * @param {number} value - Value to set the field to if it passes the
+   *   comparison test.
+   * @returns {Array} - array of parameters that evalsha can use to execute
+   *   the script.
+   */
+  updateIfLess,
+};
---
+++ tests/1-test_single_she.test.ts
@@ -0,0 +1,47 @@
+import { getRedisClient } from "../src/config/redis.config";// const config = require("better-config");
+
+// config.set("../config.json");
+
+// const redis = redisClient;
+
+const testSuiteName = "site_dao_redis_impl";
+
+const testKeyPrefix = `test:${testSuiteName}`;
+
+// keyGenerator.setPrefix(testKeyPrefix);
+
+// pnpm run test -- 1-test_single_she.test
+/* eslint-disable no-undef */
+describe("AppController (e2e)", async () => {
+  const client = await getRedisClient();
+
+  beforeAll(() => {
+    jest.setTimeout(60000);
+  });
+
+  afterEach(async () => {
+    const testKeys = await client.keys(`${testKeyPrefix}:*`);
+
+    if (testKeys.length > 0) {
+      await client.del(testKeys);
+    }
+  });
+
+  afterAll(() => {
+    // Release Redis connection.
+    client.quit();
+  });
+
+  test(`${testSuiteName}: lua test`, async () => {
+    
+
+  // Load script and get its SHA back.
+  const sha = await updateIfLowestScript.load();
+
+  // Execute script (1 = number of Redis keys).
+  const result = await client.evalshaAsync(sha, 1, key, value);
+
+  // Check result.
+  expect(result).toBe(1);
+  });
+});
