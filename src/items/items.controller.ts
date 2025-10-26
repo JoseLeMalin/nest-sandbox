0efdcf306ca35d49860f1c089c6c4b4d0da39376
JoseLeMalin <seb_488@hotmail.fr>
2025-10-26 13:57:14 +0100

    a commit a day

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,1 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";import { ItemsService } from "./items.service";import { UpdateItemDto } from "./dto/update-item.dto";import { CreateItemDtoInput } from "./dto/create-item.dto";@Controller("items")export class ItemsController {  constructor(private readonly itemsService: ItemsService) {}  @Post()  create(@Body() createItemDto: CreateItemDtoInput) {    return this.itemsService.create(createItemDto);  }  @Get()  findAll() {    return this.itemsService.findAll();  }  @Get(":id")  findOne(@Param("id") id: string) {    return this.itemsService.findOne(+id);  }  @Patch(":id")  update(@Param("id") id: string, @Body() updateItemDto: UpdateItemDto) {    return this.itemsService.update(+id, updateItemDto);  }  @Delete(":id")  remove(@Param("id") id: string) {    return this.itemsService.remove(+id);  }}
+import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { ItemsService } from "./items.service";
+
+import { UpdateItemDto } from "./dto/update-item.dto";
+import { CreateItemDtoInput } from "./dto/create-item.dto";
+
+@Controller("items")
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  
+  @Post()
+  create(@Body() createItemDto: CreateItemDtoInput) {
+    return this.itemsService.create(createItemDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.itemsService.findAll();
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.itemsService.findOne(+id);
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateItemDto: UpdateItemDto) {
+    return this.itemsService.update(+id, updateItemDto);
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.itemsService.remove(+id);
+  }
+}
