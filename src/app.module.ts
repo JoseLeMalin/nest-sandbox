734d2e9bd66500a95bd2a32bcf5dc15d36ba4223
José LeMalin <jlm_488@tutamail.com>
2026-01-27 11:05:43 +0100

    Added new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { VotesModule } from "./votes/votes.module";
+import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [UsersModule],
+  imports: [AuthModule, VotesModule, UsersModule],
   controllers: [],
   providers: [],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { Pool } from "pg";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private pool = new Pool();
+  private readonly users = [
+    {
+      userId: 1,
+      username: "john",
+      password: "changeme",
+    },
+    {
+      userId: 2,
+      username: "maria",
+      password: "guess",
+    },
+  ];
+  async create(createUserDto: CreateUserDto) {
+    const client = await this.pool.connect();
+    try {
+      const newUser = client.query<{ username: string; email: string; full_name: string }>(
+        "INSERT INTO users(username, password, email, full_name) VALUES($1, $2, $3, $4) RETURNING *",
+        [
+          createUserDto.username,
+          createUserDto.password,
+          createUserDto.email,
+          createUserDto.fullName,
+        ],
+      );
+      return newUser;
+    } finally {
+      client.release();
+    }
+  }
+
+  async findAll() {
+    const client = await this.pool.connect();
+    try {
+      const res = await client.query<{ username: string; email: string; full_name: string }>(
+        "SELECT username, email, full_name FROM users",
+      );
+      return res.rows;
+    } finally {
+      client.release();
+    }
+  }
+
+  async findOne(id: number) {
+    const client = await this.pool.connect();
+    try {
+      const res = await client.query<{ username: string; email: string; full_name: string }>(
+        "SELECT username, email, full_name FROM users WHERE user_id = $1",
+        [id],
+      );
+      return res.rows[0];
+    } finally {
+      client.release();
+    }
+  }
+  async findOneByUsername(username: string) {
+    const client = await this.pool.connect();
+    try {
+      const res = await client.query<{ userId: number; username: string; password: string }>(
+        "SELECT user_id, username, password FROM users WHERE username = $1",
+        [username],
+      );
+      return res.rows[0];
+    } finally {
+      client.release();
+    }
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  async remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
